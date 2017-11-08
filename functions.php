9ba4e5adcb7807dec85433a1c3ed0e6b767c74c0
https://github.com/mariacetinic/html5.git <maria.cetinic@gmail.com>
2017-11-08 13:50:00 +0100

    uppgift 5

--- functions.php
+++ functions.php
@@ -1,8 +1,28 @@
 <?php
 // "HTTP/1.0 404 Not Found"
-function status_header($code = 200) {}
+function status_header($code = 200) {
+    $messages = [
+        200 => "OK",
+        404 => "Not found"
+    ];
+    header("HTTP/1.1 ".$code." ".$messages[$code]);
+}
+echo status_header(); //standard är 200
 
 // [header => värde]
-function headers(array $headers = []) {}
+
+
+
+function headers(array $headers = []){
+    //header ("connection: keep alive");
+    foreach ($headers as $header => $value) {
+        echo "Status: $value <br>";
+    }
+}
+
+headers([
+    "Connection" => "keep-alive",
+    "Waiting" => "keep-waiting"
+]);
 
 function redirect($url, $code = 302) {}
